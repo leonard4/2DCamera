5992e2fa621cd98530275bb20305be0a4f3f49cc
Leonard Burns <leonard4@gmail.com>
2015-01-13 19:46:14 -0800

    Fixed camera movement by using a threshold value

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,46 +17,106 @@ public class Player : MonoBehaviour {
 	void Start () {
 
 	}
-	
+
+    void OnEnable()
+    {
+        StartCoroutine(PostFixedUpdate());
+    }
+
+    IEnumerator PostFixedUpdate()
+    {
+        while (true)
+        {
+            yield return new WaitForFixedUpdate();
+
+            updateCamera();
+        }
+    }
+
 	// Update is called once per frame
 	void Update () {
-        Vector3 viewPos = Camera.main.WorldToViewportPoint(transform.position);
+        //Vector3 viewPos = Camera.main.WorldToViewportPoint(transform.position);
 
-        //Debug.Log(viewPos);
+        ////Debug.Log(viewPos);
 
-        if (viewPos.x >= 0.95f)
-        {
-            // Smoothly catch up
-            mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, new Vector3(transform.position.x, mainCamera.transform.position.y, mainCamera.transform.position.z), cameraSpeed * Time.deltaTime);
-            // Snap to next screen
-            //mainCamera.transform.position = new Vector3(transform.position.x, mainCamera.transform.position.y, mainCamera.transform.position.z);
-            Debug.Log("Right");
-        }
+        //if (viewPos.x >= 0.95f)
+        //{
+        //    // Smoothly catch up
+        //    mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, new Vector3(transform.position.x, mainCamera.transform.position.y, mainCamera.transform.position.z), cameraSpeed * Time.deltaTime);
+        //    // Snap to next screen
+        //    //mainCamera.transform.position = new Vector3(transform.position.x, mainCamera.transform.position.y, mainCamera.transform.position.z);
+        //    Debug.Log("Right");
+        //}
 
-        if (viewPos.x <= 0.05f)
-        {
-            mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, new Vector3(transform.position.x, mainCamera.transform.position.y, mainCamera.transform.position.z), cameraSpeed * Time.deltaTime);
-            //mainCamera.transform.position = new Vector3(transform.position.x, mainCamera.transform.position.y, mainCamera.transform.position.z);
-            Debug.Log("Left");
-        }
+        //if (viewPos.x <= 0.05f)
+        //{
+        //    mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, new Vector3(transform.position.x, mainCamera.transform.position.y, mainCamera.transform.position.z), cameraSpeed * Time.deltaTime);
+        //    //mainCamera.transform.position = new Vector3(transform.position.x, mainCamera.transform.position.y, mainCamera.transform.position.z);
+        //    Debug.Log("Left");
+        //}
 
-        if (viewPos.y <= 0.1f)
-        {
-            mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, new Vector3(mainCamera.transform.position.x, transform.position.y, mainCamera.transform.position.z), cameraSpeed * Time.deltaTime);
-            //mainCamera.transform.position = new Vector3(mainCamera.transform.position.x, transform.position.y, mainCamera.transform.position.z);
-            Debug.Log("Top");
-        }
+        //if (viewPos.y <= 0.1f)
+        //{
+        //    mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, new Vector3(mainCamera.transform.position.x, transform.position.y, mainCamera.transform.position.z), cameraSpeed * Time.deltaTime);
+        //    //mainCamera.transform.position = new Vector3(mainCamera.transform.position.x, transform.position.y, mainCamera.transform.position.z);
+        //    Debug.Log("Top");
+        //}
 
-        if (viewPos.y >= 0.9f)
-        {
-            mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, new Vector3(mainCamera.transform.position.x, transform.position.y, mainCamera.transform.position.z), cameraSpeed * Time.deltaTime);
-            //mainCamera.transform.position = new Vector3(mainCamera.transform.position.x, transform.position.y, mainCamera.transform.position.z);
-            Debug.Log("Bottom");
-        }
+        //if (viewPos.y >= 0.9f)
+        //{
+        //    mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, new Vector3(mainCamera.transform.position.x, transform.position.y, mainCamera.transform.position.z), cameraSpeed * Time.deltaTime);
+        //    //mainCamera.transform.position = new Vector3(mainCamera.transform.position.x, transform.position.y, mainCamera.transform.position.z);
+        //    Debug.Log("Bottom");
+        //}
         
         // Get our horizontal and vertical inputs and multiply them by a speed variable
         playerMovement = new Vector2(speed.x * Input.GetAxis("Horizontal"), speed.y * Input.GetAxis("Vertical"));
 	}
+
+    public float horizontalThreshold = 0.4f;
+    public float verticalThreshold = 0.4f;
+
+    void updateCamera()
+    {
+        Vector3 viewPos = Camera.main.WorldToViewportPoint(transform.position);
+        Debug.Log("viewPos = " + viewPos.ToString());
+        //Debug.Log(viewPos);
+
+        bool dirty = false;
+
+        if (viewPos.x > (1f - horizontalThreshold))
+        {
+            viewPos.x = (1f - horizontalThreshold);
+            Debug.Log("Right");
+            dirty = true;
+        }
+
+        if (viewPos.x < horizontalThreshold)
+        {
+            viewPos.x = horizontalThreshold;
+            Debug.Log("Left");
+            dirty = true;
+        }
+
+        if (viewPos.y < verticalThreshold)
+        {
+            viewPos.y = verticalThreshold;
+            dirty = true;
+        }
+
+        if (viewPos.y > (1f - verticalThreshold))
+        {
+            viewPos.y = (1f - verticalThreshold);
+            dirty = true;
+        }
+
+        if (dirty)
+        {
+            // figure out how much the camera needs to move so that the player will be at the updated viewport location
+            Vector3 offset = transform.position - Camera.main.ViewportToWorldPoint(viewPos);
+            mainCamera.transform.position += offset;
+        }
+    }
 
     void FixedUpdate()
     {
